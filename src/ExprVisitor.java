cd130fc476859abde458469c0dd462c3b30ea346
rnujella <rnujella@binghamton.edu>
2024-10-07 16:50:37 -0400

    Initial commit

---
+++ src/ExprVisitor.java
@@ -0,0 +1,13 @@
+package src;
+
+public interface ExprVisitor<R> {
+    /*
+     * Part 2: Implement the visitor pattern for expressions
+     */
+
+    R visitPlusExpr(PlusExpr expr);
+    R visitMinusExpr(MinusExpr expr);
+    R visitTimesExpr(TimesExpr expr);
+    R visitDivExpr(DivExpr expr);
+    R visitFloatExpr(FloatExpr expr);
+}
